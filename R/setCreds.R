45ce33487417751adf37f141cb33d5b134bde5e5
ek271971 <xrunxcx@gmail.com>
2015-01-23 13:46:17 -0500

    Credential storage feedback.

--- R/setCreds.R
+++ R/setCreds.R
@@ -2,4 +2,5 @@ cacheEnv <- new.env()
 
 setCreds <- function(user,pass){
   assign("creds",list(user,pass), envir=cacheEnv)
+  message("API credentials saved. You may now use package functions.")
 }
